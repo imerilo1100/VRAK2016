bb440d8808f7a6ac3280ac1f34a59eb8a1663bd2
Herko Pirk <herko.pirk@gmail.com>
2016-03-06 23:31:48 +0200

    location edited

--- index.php
+++ index.php
@@ -1,2 +1,38 @@
 <?php
-echo "Hello world!";
+
+?>
+<head>
+    <title>E-Hääletus</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <style type="text/css">
+    @import "./style/defaultstyle.css";
+    </style>
+</head>
+
+<div id="header">
+	<h1>Tere tulemast e-hääletamiste lehele!</h1>
+	<table><tr>
+			<td><a href="./pages/textpage.html">Valimistest</a></td>
+			<td><a href="./pages/kandidaadid.html">Kandidaatide nimekiri</a></td>
+			<td><a href="./pages/textpage.html">Abi</a></td>
+			<td><a href="./">Eesti</a></td>
+			<td><a href="./pages/englishindex.html">English</a></td>
+	</tr></table>
+</div>
+
+<div id="contentcontainer">
+	<div id="logininfo"><p>Siia tuleb lühike selgitav tekst sisselogimisest, valimisest vms, võibolla pilt taustaks.</p><p>tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst</p><p>tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst-tekst</p></div>
+	<div id="loginfields">
+		<div id="togglebuttons">
+			<button class="togglebtn">ID-Kaart</button><!--
+	 	 --><button class="togglebtn">Digi-ID</button><!--
+	 	 --><button class="togglebtn">Mobiil-ID</button>
+		</div>
+		<form action="./pages/loggedin.html">
+			<br><br>
+			<b>Salasõna: </b><br>
+			<input type="text"><br><br>
+			<input type="submit" value="Autendi">
+		</form>
+	</div>
+</div>
