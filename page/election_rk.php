14379fecf4817b023c2f0f15252106450013aa75
imerilo1100 <i.merilo.1100@gmail.com>
2016-05-01 23:39:40 +0300

    nesting fix~

--- page/election_rk.php
+++ page/election_rk.php
@@ -1,7 +1,4 @@
-<?php require 'page/election_header.php'; ?>
-
 <section id="content">
 	<?php require 'election_content/rk_info.php';?>
 </section>
-	
-<?php require 'page/election_footer.php'; ?>
+
