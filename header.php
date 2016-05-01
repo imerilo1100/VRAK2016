08bc508b0e2821d160941b49852508553a748eba
imerilo1100 <i.merilo.1100@gmail.com>
2016-05-02 00:19:47 +0300

    stylesheet href

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="google-signin-scope" content="profile email">
     <meta name="google-signin-client_id" content="313727625383-j60j93a2g1lk9iajl2d404mkb7e1ij2q.apps.googleusercontent.com">
-    <style type="text/css"> @import "./css/defaultstyle.css"; </style>
+	<link rel=StyleSheet href="css/defaultstyle.css" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.js"></script>
     <script src="/js/main.js"></script>
