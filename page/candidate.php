1c7a4e3c5dcd27e69b78122386e760b75795666c
imerilo1100 <i.merilo.1100@gmail.com>
2016-05-02 01:08:58 +0300

    nofetch

--- page/candidate.php
+++ page/candidate.php
@@ -1,5 +1,10 @@
 <?php
-	include "../data/config.php";
+	$db = pg_connect("dbname=d8m5d1ggv02dvb 
+					  host=ec2-54-247-167-90.eu-west-1.compute.amazonaws.com 
+					  port=5432 
+					  user=mcnptqegvzaixb 
+					  password=lGS_pncoLTlIx5DzMybQxA4_R2 
+					  sslmode=require");
 	$voting = "";
 	if(isset($_POST["select_voting"])) {$voting = $_POST["voting"];}
 ?>
