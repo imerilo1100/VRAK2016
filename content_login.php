214887697506772dc8b22a6183a6f5edb9d57f72
imerilo1100 <i.merilo.1100@gmail.com>
2016-05-02 00:44:19 +0300

    session check

--- content_login.php
+++ content_login.php
@@ -1,6 +1,6 @@
 <?php
 	include "data/config.php";
-	if isset($_SESSION["login_user"]) {$logged_user = pg_escape_string($_SESSION["login_user"]);}
+	if (isset($_SESSION["login_user"])) {$logged_user = pg_escape_string($_SESSION["login_user"]);}
 	if($db){
 		$res = pg_query($db, "SELECT username FROM person WHERE username ='".$logged_user."'");
 		$rw = pg_fetch_assoc($res);
