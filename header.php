e018019bf31b17e4eac775a847c877a44d701932
andrepeedosaar <andre190@ut.ee>
2016-05-08 13:52:10 +0300

    min

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 	<meta name=viewport content="width=device-width, initial-scale=1">
     <meta name="google-signin-scope" content="profile email">
     <meta name="google-signin-client_id" content="313727625383-j60j93a2g1lk9iajl2d404mkb7e1ij2q.apps.googleusercontent.com">
-	<link rel=StyleSheet href="css/defaultstyle.css" type="text/css">
+	<link rel=StyleSheet href="css/defaultstyle_min.css" type="text/css">
     <script	async src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script async src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
     <script async src="/js/main_min.js"></script>
