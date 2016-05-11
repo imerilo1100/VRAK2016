6f4bf4aab0cb23fdd073feb58280405b3e01a27e
andrepeedosaar <andre190@ut.ee>
2016-05-11 10:01:05 +0300

    async delete

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta name="google-signin-scope" content="profile email">
     <meta name="google-signin-client_id" content="313727625383-j60j93a2g1lk9iajl2d404mkb7e1ij2q.apps.googleusercontent.com">
 	<link rel=StyleSheet href="css/defaultstyle_min.css" type="text/css">
-    <script	async src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script async src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-    <script async src="/js/main_min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="/js/main_min.js"></script>
 </head>
